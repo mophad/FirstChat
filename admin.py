0c7c44771bd6b68bc605bac47acdfe54519d3d32
Robert <142910860+mophad@users.noreply.github.com>
2023-09-19 09:19:12 -0200

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from .models import UserModel, User
+
+# Register your models here.
+admin.site.register(UserModel)
+admin.site.register(User)
+
+# Register your models here.
