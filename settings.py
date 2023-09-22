8e87c2050dbd32d5b8555fe1fe58f123ab46ca86
Robert <142910860+mophad@users.noreply.github.com>
2023-09-22 15:11:30 -0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'signup',
-    'django_bootstrap5'
+    'django_bootstrap5',
 
 ]
 
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage' 
 
 ROOT_URLCONF = 'firstchat.urls'
 
@@ -129,6 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'signup/static'),
     os.path.join(BASE_DIR, 'assets'),
+    BASE_DIR / 'assets'
 
 ]
 
@@ -136,3 +139,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# AUTH_USER_MODEL = 'signup.Login_E'  
