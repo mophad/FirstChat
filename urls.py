3eb518277ddb0e18ee8118a398026d7dd5f33644
Robert <142910860+mophad@users.noreply.github.com>
2023-10-01 14:46:54 -0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -15,6 +15,17 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+ path('bot', views.chatbot, name='bot'),
+    path('chat', views.chatboard, name='chat'),
+    path('lead', views.lead, name='lead'),
+    path('setting', views.setting, name='setting'),
+    path('set_bot', views.botsetting, name='set_bot'),
+    path('set_app', views.appsetting, name='set_app'),
+    path('integrate', views.integrat, name='integrate'),
+    path('add_user', views.add_user, name='add_user'),
+    path('add_company', views.add_company, name='add_company'),
+    # path('customize', views.custome_app, name='customize'),
+
 
 
 ]
