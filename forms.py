7a601b3ddc70a1fff41d3a38db59ac420ab1fcba
Robert <142910860+mophad@users.noreply.github.com>
2023-10-01 14:41:21 -0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -20,4 +20,50 @@ class SignupForm(ModelForm):
             'password': forms.PasswordInput(attrs={'class': 'form-control' , 'placeholder': 'Enter your password.', 'name':'password'})
         }
 
+class App_Customize(ModelForm):
+
+    class Meta:
+        model = Page_Setting
+        fields = [
+            "title",
+            "footertext",
+            "address",
+            "email",
+            "number",
+            "favicon",
+            "pagelogo",
+            "signuplogo",
+            
+        ]
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control',
+                                             'placeholder': 'Enter your password',
+                                               'name':'fullname'}),
+            'footertext': forms.TextInput(attrs={'class': 'form-control',
+                                                 'placeholder': 'Please enter a valid username!',
+                                                   'name':'username'}),
+            'address': forms.TextInput(attrs={'class': 'form-control' ,
+                                                'placeholder': 'Please, enter your email!', 
+                                                  'name':'email'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control' , 
+                                                'placeholder': 'Enter your password.',
+                                                  'name':'password'}),
+            'number': forms.NumberInput(attrs={'class': 'form-control' ,
+                                                  'placeholder': 'Enter your password.',
+                                                    'name':'password'}),
+            'favicon': forms.FileInput(attrs={'class': 'form-control' , 
+                                                  'placeholder': 'Enter your password.',
+                                                    'name':'password'}),
+            'pagelogo': forms.FileInput(attrs={'class': 'form-control' , 
+                                                   'placeholder': 'Enter your password.', 
+                                                   'name':'password'}),
+            'signuplogo': forms.FileInput(attrs={'class': 'form-control' ,
+                                                      'placeholder': 'Enter your password.',
+                                                        'name':'password'}),
+
+
+
+        }
+
 
