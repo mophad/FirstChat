b6754456ab1f4f148de8fbc928c2eb16ca0b5a6d
Robert <142910860+mophad@users.noreply.github.com>
2023-10-01 14:48:42 -0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,11 +1,14 @@
 
 from django.contrib import admin
-from .models import UserModel, User, Question , Choice
+from .models import UserModel, User, Question , Choice, Article , Page_Setting
 
 # Register your models he
 admin.site.register(User)
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Article)
+admin.site.register(Page_Setting)
+
 
 
 # Register your models here.
