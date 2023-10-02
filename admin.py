391f4a074b6b407152ae1f2fe00932e21b7bcc2f
Robert <142910860+mophad@users.noreply.github.com>
2023-10-02 12:14:59 -0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from .models import UserModel, User, Question , Choice, Article , Page_Setting
+from .models import UserModel, User, Question , Choice, Article,Page_Setting, Create_Acct , Create_Company, Open_Ai ,Bot_Acct
 
 # Register your models he
 admin.site.register(User)
@@ -8,6 +8,11 @@ admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Article)
 admin.site.register(Page_Setting)
+admin.site.register(Create_Acct)
+admin.site.register(Create_Company)
+admin.site.register(Open_Ai)
+admin.site.register(Bot_Acct)
+
 
 
 
